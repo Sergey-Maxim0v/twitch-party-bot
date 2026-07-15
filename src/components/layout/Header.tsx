94c1eef57abf2b998c999dfaed39205570286b7d
SM <dev9092336062@gmail.com>
2026-07-16 00:43:44 +0300

    feat(layout): add twitch icon

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import {ThemeToggle} from './ThemeToggle';
+import {LuTwitch} from "react-icons/lu";
 
 export const Header: React.FC = () => {
     return (
         <header className="navbar bg-base-200 border-b border-base-300 px-6 flex justify-between items-center">
+            <LuTwitch className="mr-2 text-2xl font-black text-primary select-none"/>
+
             <div className="flex-1">
-        <span className="text-xl font-black tracking-tight text-primary select-none">
-          Twitch Party Bot
-        </span>
+                <span className="text-xl font-black tracking-tight text-primary select-none">
+                  Twitch Party Bot
+                </span>
             </div>
 
             <div className="flex-none">
