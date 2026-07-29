c9d1645f21c8e130feff2d75d996d287cadc94e3
SM <dev9092336062@gmail.com>
2026-07-29 14:17:20 +0300

    refactor

--- src/features/auth/utils/index.ts
+++ src/features/auth/utils/index.ts
@@ -1,6 +1,5 @@
 export * from './extractTwitchToken';
 export * from './getTwitchAuthUrl';
-export * from './checkAuthError';
 export * from './crypto';
 export * from './getTwitchAuthUrl';
 export * from './validateTwitchToken';
